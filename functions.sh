a425ea5834f611623c7084be037da7d276c004a2
andryeltj <66152615+andryeltj@users.noreply.github.com>
2023-01-29 13:04:42 -0300

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -327,12 +327,14 @@ get_icon()
   find ./usr/share/icons -path *128* -name $LOWERAPP.png -exec cp {} . \; 2>/dev/null || true
   find ./usr/share/icons -path *512* -name $LOWERAPP.png -exec cp {} . \; 2>/dev/null || true
   find ./usr/share/icons -path *256* -name $LOWERAPP.png -exec cp {} . \; 2>/dev/null || true
+  find ./usr/share/icons -path *scalable* -name $LOWERAPP.png -exec cp {} . \; 2>/dev/null || true
   ls -lh $LOWERAPP.png || true
   find ./usr/share/pixmaps/$LOWERAPP.svg -exec cp {} . \; 2>/dev/null || true
   find ./usr/share/icons -path *64* -name $LOWERAPP.svg -exec cp {} . \; 2>/dev/null || true
   find ./usr/share/icons -path *128* -name $LOWERAPP.svg -exec cp {} . \; 2>/dev/null || true
   find ./usr/share/icons -path *512* -name $LOWERAPP.svg -exec cp {} . \; 2>/dev/null || true
   find ./usr/share/icons -path *256* -name $LOWERAPP.svg -exec cp {} . \; 2>/dev/null || true
+  find ./usr/share/icons -path *scalable* -name $LOWERAPP.svg -exec cp {} . \; 2>/dev/null || true
   ls -lh $LOWERAPP.svg || true
 }
 
